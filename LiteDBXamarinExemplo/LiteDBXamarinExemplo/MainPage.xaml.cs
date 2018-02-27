34469ae38e9fce065db05f672954e92225887cd8
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-02-27 11:20:18 -0300

    Primeira Versão

--- LiteDBXamarinExemplo/LiteDBXamarinExemplo/MainPage.xaml.cs
+++ LiteDBXamarinExemplo/LiteDBXamarinExemplo/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiteDB;
+using LiteDBXamarinExemplo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +15,38 @@ namespace LiteDBXamarinExemplo
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void btnGerarPorco_Clicked(object sender, EventArgs e)
+        {
+            LiteRepository repositorio = new LiteRepository("MeuBanco.db");
+
+            Porco porco = new Porco
+            {
+                Nome = "Rodolfo",
+                Idade = 3,
+                Apelidos = new string[] { "Rodie", "Rodolfinho" },
+                VirouBacon = false
+            };
+
+            repositorio.Insert<Porco>(porco);
+
+            lStatusPorco.Text = "Porco Criado!";
+    }
+        private void btnTransformaBacon_Clicked(object sender, EventArgs e)
+        {
+            LiteRepository repositorio = new LiteRepository("MeuBanco.db");
+
+           List<Porco> Porcos = repositorio.Query<Porco>().ToList();
+
+            if (Porcos.Count > 0)
+            {
+                var porco = Porcos[0];
+                porco.VirouBacon = true;
+                repositorio.Update<Porco>(porco);
+
+                lStatusPorco.Text = "Virou Bacon";
+            }
+
+        }
+    }
 }
---
+++ LiteDBXamarinExemplo/LiteDBXamarinExemplo/Models/Porco.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiteDBXamarinExemplo.Models
+{
+    public class Porco
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string[] Apelidos { get; set; }
+        public bool VirouBacon { get; set; }
+    }
+}
