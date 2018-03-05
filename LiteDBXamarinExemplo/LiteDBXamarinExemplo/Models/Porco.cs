d6fbfcd9942f09be4b91a33878f32495c21d0605
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-03-05 09:10:48 -0300

    Finalizado Exemplo de LiteDB

--- LiteDBXamarinExemplo/LiteDBXamarinExemplo/Models/Porco.cs
+++ LiteDBXamarinExemplo/LiteDBXamarinExemplo/Models/Porco.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using LiteDB;
 
 namespace LiteDBXamarinExemplo.Models
 {
     public class Porco
     {
+        [BsonId]
+        public int id { get; set; }
         public string Nome { get; set; }
         public int Idade { get; set; }
         public string[] Apelidos { get; set; }
